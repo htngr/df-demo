7a9d7d73336656bee18ba63607245d7e36f21d34
htngr <124245785+htngr@users.noreply.github.com>
2025-05-07 09:39:23 +0000

    added spring & angular

--- configuration.nix
+++ configuration.nix
@@ -6,13 +6,25 @@
 
   programs.npm.enable = true;
 
+  codchi.keyring.enable = true; # allow intellij to remember passwords
 
   environment.systemPackages = [
     pkgs.jetbrains.idea-ultimate
     pkgs.nodejs
+    pkgs.nodePackages."@angular/cli"
+    pkgs.ungoogled-chromium
   ];
 
   # For proprietary apps like IntelliJ Ultimate
   nixpkgs.config.allowUnfree = true;
+
+  services.postgresql = {
+    enable = true;
+    initialScript = pkgs.writeText "init-sql-script" ''
+      ALTER USER postgres WITH PASSWORD 'postgres';
+    '';
+    ensureDatabases = [ "postgres" ];
+    enableTCPIP = true;
+  };
 }
 
